712506d57a3fd0335d20b1ee40028e251673d898
35097602 <62830921+35097602@users.noreply.github.com>
2022-05-08 15:33:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,31 @@
-xy = (y[..., 0:2] * 2 - 0.5 + self.grid[i]) * self.stride[i]  # xy
-wh = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+static void gemm_v1(float* matA, float* matB, float* matC, const int M, const int N, const int K, const int strideA, const int strideB, const int strideC)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			float sum = 0.0f;
+			for (int k = 0; k < K; k++)
+			{
+				sum += matA[j*strideA + k] * matB[k*strideB + i];
+			}
+			matC[j*strideC + i] = sum;
+		}
+	}
+}
+
+static void gemm_v2(float* matA, float* matB, float* matC, const int M, const int N, const int K, const int strideA, const int strideB, const int strideC)
+{
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			float sum = 0.0f;
+			for (int k = 0; k < K; k++)
+			{
+				sum += matA[i*strideA + k] * matB[k*strideB + j];
+			}
+			matC[i*strideC + j] = sum;
+		}
+	}
+}
